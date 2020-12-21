7c350dde9035cf3afc7b605319f1f4c009b39069
WerWolv <werwolv98@gmail.com>
2020-12-21 12:38:50 +0100

    Added current commit hash and branch to about page

--- source/views/view_help.cpp
+++ source/views/view_help.cpp
@@ -30,6 +30,7 @@ namespace hex {
     void ViewHelp::drawAboutPopup() {
         if (ImGui::BeginPopupModal("About", &this->m_aboutWindowOpen, ImGuiWindowFlags_AlwaysAutoResize)) {
             ImGui::Text("ImHex Hex Editor v%s by WerWolv", IMHEX_VERSION);
+            ImGui::Text("%s@%s", GIT_BRANCH, GIT_COMMIT_HASH);
             ImGui::NewLine();
             ImGui::Text("Source code available on GitHub:"); ImGui::SameLine();
             ImGui::TextColored(ImVec4(0.4F, 0.4F, 0.8F, 1.0F), "WerWolv/ImHex   ");
