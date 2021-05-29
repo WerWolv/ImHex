c373174436a01427221a7c2c6762d1148fd99708
WerWolv <werwolv98@gmail.com>
2021-05-29 21:52:18 +0200

    plugins: Added windows-only features plugin

---
+++ plugins/windows/source/plugin_windows.cpp
@@ -0,0 +1,10 @@
+#include <hex/plugin.hpp>
+#include <imgui.h>
+#include <imgui_internal.h>
+
+#include "views/view_tty_console.hpp"
+
+IMHEX_PLUGIN_SETUP("Windows", "WerWolv", "Windows-only features") {
+}
+
+
