92bb8529214bc610cbbbec250853521c2de9dd75
WerWolv <werwolv98@gmail.com>
2021-07-31 17:46:09 +0200

    fix: Resource embedding not working on Windows

--- plugins/libimhex/source/resources.cpp
+++ plugins/libimhex/source/resources.cpp
@@ -1,15 +1,32 @@
-#define RESOURCE(name, path) __asm__ (      \
-".section .rodata\n"                        \
-".global " #name "\n"                       \
-".global " #name "_size\n"                  \
-    #name ":\n"                             \
+#if defined(OS_WINDOWS)
+    #define RESOURCE(name, path) __asm__ (      \
+    ".section .rodata\n"                        \
+    ".global " #name "\n"                       \
+    ".global " #name "_size\n"                  \
+        #name ":\n"                             \
+        ".incbin \"" path "\"\n"                \
+        #name "_size:\n"                        \
+        ".int " #name "_size - " #name "\n"     \
+        ".align 8\n"                            \
+        )
+#else
+
+    #define RESOURCE(name, path) __asm__ (      \
+    ".section .rodata\n"                        \
+    ".global " #name "\n"                       \
+    ".global " #name "_size\n"                  \
+        #name ":\n"                             \
         ".incbin \"" path "\"\n"            \
         ".type " #name ", @object\n"        \
         ".size " #name "_size, 1\n"         \
-    #name "_size:\n"                        \
+        #name "_size:\n"                        \
         ".int " #name "_size - " #name "\n" \
         ".align 8\n"                        \
-    )
+        )
+
+#endif
+
+
 
 RESOURCE(banner_light, "../../../res/resources/banner_light.png");
 RESOURCE(banner_dark, "../../../res/resources/banner_dark.png");
