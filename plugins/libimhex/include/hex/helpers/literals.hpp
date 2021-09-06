d7707bae62e21b3f058077c7040214d5df5c765e
WerWolv <werwolv98@gmail.com>
2021-09-06 23:08:02 +0200

    fix: Multiple definitions of literals

--- plugins/libimhex/include/hex/helpers/literals.hpp
+++ plugins/libimhex/include/hex/helpers/literals.hpp
@@ -4,19 +4,19 @@ namespace hex::literals {
 
     /* Byte literals */
 
-    unsigned long long operator ""_Bytes(unsigned long long bytes) {
+    constexpr static inline unsigned long long operator ""_Bytes(unsigned long long bytes) noexcept {
         return bytes;
     }
 
-    unsigned long long operator ""_kiB(unsigned long long kiB) {
+    constexpr static inline unsigned long long operator ""_kiB(unsigned long long kiB) noexcept {
         return operator ""_Bytes(kiB * 1024);
     }
 
-    unsigned long long operator ""_MiB(unsigned long long MiB) {
+    constexpr static inline unsigned long long operator ""_MiB(unsigned long long MiB) noexcept {
         return operator ""_kiB(MiB * 1024);
     }
 
-    unsigned long long operator ""_GiB(unsigned long long GiB) {
+    constexpr static inline unsigned long long operator ""_GiB(unsigned long long GiB) noexcept {
         return operator ""_MiB(GiB * 1024);
     }
 
