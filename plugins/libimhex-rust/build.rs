007fedd32a632290e29214360d3335fba37ac600
WerWolv <werwolv98@gmail.com>
2021-10-03 13:45:59 +0200

    build: Enable concepts for cxx.rs

--- plugins/libimhex-rust/build.rs
+++ plugins/libimhex-rust/build.rs
@@ -1,13 +1,14 @@
 fn main() {
-    println!("cargo:rustc-link-lib=dylib=libimhex");
-    println!("cargo:rustc-link-search=native={}", env!("LIBIMHEX_OUTPUT_DIRECTORY"));
+    println!("cargo:rustc-link-lib=dylib=imhex");
+    println!("cargo:rustc-link-search=all={}", env!("LIBIMHEX_OUTPUT_DIRECTORY"));
 
     println!("cargo:rerun-if-changed=src/lib.rs");
     println!("cargo:rerun-if-changed=src/imhex_api.rs");
 
     cxx_build::bridge("src/imhex_api.rs")
         .include(format!("{}/include", env!("LIBIMHEX_SOURCE_DIRECTORY")))
-        .flag_if_supported("-std=gnu++2a")
         .flag_if_supported("-std=gnu++20")
+        .flag_if_supported("-std=gnu++2a")
+        .flag_if_supported("-fconcepts")
         .compile("libimhex-bridge");
 }
