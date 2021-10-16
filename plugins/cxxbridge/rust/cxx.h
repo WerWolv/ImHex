b97dbf213959a4c8ad98219bf8eaabe49dc7a80c
WerWolv <werwolv98@gmail.com>
2021-10-16 11:04:56 +0200

    rust: Cleanup

--- plugins/cxxbridge/rust/cxx.h
+++
@@ -1,1 +0,0 @@
-/home/jam/.cargo/registry/src/github.com-1ecc6299db9ec823/cxx-1.0.55/include/cxx.h
--- plugins/example_rust/source/plugin_example.rs
+++ plugins/example_rust/source/plugin_example.rs
@@ -1,20 +1,7 @@
 extern crate hex;
 
-#[hex::plugin_setup("Hello Rust", "WerWolv", "Test Description")]
+#[hex::plugin_setup("Example Rust", "WerWolv", "Example Rust plugin used as template for plugin devs")]
 fn init() {
-    println!("Hello from Rust!");
 
-    hex::ImHexApi::Bookmarks::add(
-        hex::Region {
-            address: 0x00,
-            size: 0x10,
-        },
-        "Test",
-        "123",
-        None,
-    );
-
-    println!("Rust ImGui Version: {}", hex::imgui::dear_imgui_version());
-    println!("Rust ImGui Context: {:?}", hex::imgui::Context::current());
 }
 
