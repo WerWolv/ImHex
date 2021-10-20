d9a77d396cbe610f95e6b98912148942cb26c0e7
WerWolv <werwolv98@gmail.com>
2021-10-20 11:23:06 +0200

    tests: Fixed endian test being marked as failing

--- tests/algorithms/source/endian.cpp
+++ tests/algorithms/source/endian.cpp
@@ -6,12 +6,12 @@ TEST_SEQUENCE("32BitIntegerEndianSwap") {
     TEST_ASSERT(hex::changeEndianess<u32>(0xAABBCCDD, std::endian::big) == 0xDDCCBBAA);
 };
 
-TEST_SEQUENCE("64BitFloatEndianSwap", FAILING) {
+TEST_SEQUENCE("64BitFloatEndianSwap") {
     double floatValue = 1234.5;
     u64 integerValue = reinterpret_cast<u64&>(floatValue);
 
     double swappedFloatValue = hex::changeEndianess(floatValue, std::endian::big);
     u64 swappedIntegerValue = hex::changeEndianess(integerValue, std::endian::big);
 
-    TEST_ASSERT(std::memcmp(&floatValue, &integerValue, 8) && std::memcmp(&swappedFloatValue, &swappedIntegerValue, 8));
+    TEST_ASSERT(std::memcmp(&floatValue, &integerValue, 8) == 0 && std::memcmp(&swappedFloatValue, &swappedIntegerValue, 8) == 0);
 };
