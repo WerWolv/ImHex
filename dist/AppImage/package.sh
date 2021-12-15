318e42be26dbae67618eaddf2b3e9bd59195b6ba
WerWolv <werwolv98@gmail.com>
2021-12-15 21:11:41 +0100

    build: Exclude libstdc++, libc++ and ld-linux from AppImage. Search libimhex and builtin plugins for dependencies

--- dist/AppImage/package.sh
+++ dist/AppImage/package.sh
@@ -39,7 +39,9 @@ mv ${BUILDDIR}/ImHex.AppDir/AppRun-x86_64 ${APPDIR}/AppRun
 chmod a+x ${BUILDDIR}/ImHex.AppDir/AppRun
 
 ## Add all dependencies
-ldd ${BUILDDIR}/imhex | awk '/ => /{print $3}' | awk '!/(libc|libdl|libpthread|libselinux)/' | xargs -I '{}' cp '{}' ${APPDIR}/usr/lib
+ldd ${BUILDDIR}/imhex | awk '/ => /{print $3}' | awk '!/(libc|libstdc++|libc++|libdl|libpthread|libselinux|ld-linux)/' | xargs -I '{}' cp '{}' ${APPDIR}/usr/lib
+ldd ${BUILDDIR}/plugins/builtin/builtin.hexplug | awk '/ => /{print $3}' | awk '!/(libc|libstdc++|libc++|libdl|libpthread|libselinux|ld-linux)/' | xargs -I '{}' cp '{}' ${APPDIR}/usr/lib
+ldd ${BUILDDIR}/plugins/libimhex/libimhex.so | awk '/ => /{print $3}' | awk '!/(libc|libstdc++|libc++|libdl|libpthread|libselinux|ld-linux)/' | xargs -I '{}' cp '{}' ${APPDIR}/usr/lib
 
 # Package it up as described here:
 # https://github.com/AppImage/AppImageKit#appimagetool-usage
