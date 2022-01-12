63edfb8d525547ad5089e309fc61095d8c59c934
WerWolv <werwolv98@gmail.com>
2022-01-12 09:17:14 +0100

    tests: Fixed compiling of bitfield test

--- tests/pattern_language/include/test_patterns/test_pattern_bitfields.hpp
+++ tests/pattern_language/include/test_patterns/test_pattern_bitfields.hpp
@@ -10,10 +10,10 @@ namespace hex::test {
             auto testBitfield = create<PatternDataBitfield>("TestBitfield", "testBitfield", 0x12, (4 * 4) / 8, nullptr);
             testBitfield->setEndian(std::endian::big);
             testBitfield->setFields({
-                    create<PatternDataBitfieldField>("", "a", 0x12, 0, 4, nullptr),
-                    create<PatternDataBitfieldField>("", "b", 0x12, 4, 4, nullptr),
-                    create<PatternDataBitfieldField>("", "c", 0x12, 8, 4, nullptr),
-                    create<PatternDataBitfieldField>("", "d", 0x12, 12, 4, nullptr)
+                    create<PatternDataBitfieldField>("", "a", 0x12, 0, 4, testBitfield, nullptr),
+                    create<PatternDataBitfieldField>("", "b", 0x12, 4, 4, testBitfield, nullptr),
+                    create<PatternDataBitfieldField>("", "c", 0x12, 8, 4, testBitfield, nullptr),
+                    create<PatternDataBitfieldField>("", "d", 0x12, 12, 4, testBitfield, nullptr)
             });
 
             addPattern(testBitfield);
