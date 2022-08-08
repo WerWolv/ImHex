e66babfda4c1c92db456ba39513af54903137856
WerWolv <werwolv98@gmail.com>
2022-08-08 16:49:27 +0200

    patterns: Updated pattern language

--- plugins/builtin/source/content/pl_pragmas.cpp
+++ plugins/builtin/source/content/pl_pragmas.cpp
@@ -6,59 +6,6 @@
 namespace hex::plugin::builtin {
 
     void registerPatternLanguagePragmas() {
-        ContentRegistry::PatternLanguage::addPragma("endian", [](pl::PatternLanguage &runtime, const std::string &value) {
-            if (value == "big") {
-                runtime.getInternals().evaluator->setDefaultEndian(std::endian::big);
-                return true;
-            } else if (value == "little") {
-                runtime.getInternals().evaluator->setDefaultEndian(std::endian::little);
-                return true;
-            } else if (value == "native") {
-                runtime.getInternals().evaluator->setDefaultEndian(std::endian::native);
-                return true;
-            } else
-                return false;
-        });
-
-        ContentRegistry::PatternLanguage::addPragma("eval_depth", [](pl::PatternLanguage &runtime, const std::string &value) {
-            auto limit = strtol(value.c_str(), nullptr, 0);
-
-            if (limit <= 0)
-                return false;
-
-            runtime.getInternals().evaluator->setEvaluationDepth(limit);
-            return true;
-        });
-
-        ContentRegistry::PatternLanguage::addPragma("array_limit", [](pl::PatternLanguage &runtime, const std::string &value) {
-            auto limit = strtol(value.c_str(), nullptr, 0);
-
-            if (limit <= 0)
-                return false;
-
-            runtime.getInternals().evaluator->setArrayLimit(limit);
-            return true;
-        });
-
-        ContentRegistry::PatternLanguage::addPragma("pattern_limit", [](pl::PatternLanguage &runtime, const std::string &value) {
-            auto limit = strtol(value.c_str(), nullptr, 0);
-
-            if (limit <= 0)
-                return false;
-
-            runtime.getInternals().evaluator->setPatternLimit(limit);
-            return true;
-        });
-
-        ContentRegistry::PatternLanguage::addPragma("loop_limit", [](pl::PatternLanguage &runtime, const std::string &value) {
-            auto limit = strtol(value.c_str(), nullptr, 0);
-
-            if (limit <= 0)
-                return false;
-
-            runtime.getInternals().evaluator->setLoopLimit(limit);
-            return true;
-        });
 
         ContentRegistry::PatternLanguage::addPragma("base_address", [](pl::PatternLanguage &runtime, const std::string &value) {
             hex::unused(runtime);
@@ -67,18 +14,6 @@ namespace hex::plugin::builtin {
 
             ImHexApi::Provider::get()->setBaseAddress(baseAddress);
             return true;
-        });
-
-        ContentRegistry::PatternLanguage::addPragma("bitfield_order", [](pl::PatternLanguage &runtime, const std::string &value) {
-            if (value == "left_to_right") {
-                runtime.getInternals().evaluator->setBitfieldOrder(pl::core::BitfieldOrder::LeftToRight);
-                return true;
-            } else if (value == "right_to_left") {
-                runtime.getInternals().evaluator->setBitfieldOrder(pl::core::BitfieldOrder::RightToLeft);
-                return true;
-            } else {
-                return false;
-            }
         });
 
         ContentRegistry::PatternLanguage::addPragma("MIME", [](pl::PatternLanguage&, const std::string &value) { return !value.empty(); });
