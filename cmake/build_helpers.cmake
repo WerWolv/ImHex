38162c01292513389cdfd6d1cb12e570f8e11b6a
iTrooz_ <itrooz@protonmail.com>
2022-08-26 00:21:17 +0200

    build: Use the bundled CA cert in AppImage (#694)
    * Added option to bundle CA
    
    * use bundled CA for AppImage
    
    * Fix bundled CA not working on Linux
    
    * revert change to add null terminated string
    
    * set IMHEX_USE_BUNDLED_CA to ON on Windows

--- cmake/build_helpers.cmake
+++ cmake/build_helpers.cmake
@@ -84,6 +84,8 @@ macro(detectOS)
         set(CMAKE_INSTALL_BINDIR ".")
         set(CMAKE_INSTALL_LIBDIR ".")
         set(PLUGINS_INSTALL_LOCATION "plugins")
+
+        SET(IMHEX_USE_BUNDLED_CA ON)
     elseif (APPLE)
         add_compile_definitions(OS_MACOS)
         set(CMAKE_INSTALL_BINDIR ".")
@@ -102,10 +104,15 @@ macro(detectOS)
             # Warning : Do not work with portable versions such as appimage (because the path is hardcoded)
             add_compile_definitions(SYSTEM_PLUGINS_LOCATION="${CMAKE_INSTALL_FULL_LIBDIR}/imhex") # "plugins" will be appended from the app
         endif()
-            
+
     else ()
         message(FATAL_ERROR "Unknown / unsupported system!")
     endif()
+
+    if(IMHEX_USE_BUNDLED_CA)
+        add_compile_definitions(IMHEX_USE_BUNDLED_CA)
+    endif()
+
 endmacro()
 
 # Detect 32 vs. 64 bit system
