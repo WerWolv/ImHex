acf6b839e5668b9af96afd4e1f3007215dd44ed9
WerWolv <werwolv98@gmail.com>
2023-01-22 12:16:22 +0100

    patterns: Updated pattern language

--- plugins/builtin/source/content/views/view_pattern_editor.cpp
+++ plugins/builtin/source/content/views/view_pattern_editor.cpp
@@ -29,7 +29,7 @@ namespace hex::plugin::builtin {
         static TextEditor::LanguageDefinition langDef;
         if (!initialized) {
             constexpr static std::array keywords = {
-                "using", "struct", "union", "enum", "bitfield", "be", "le", "if", "else", "false", "true", "this", "parent", "addressof", "sizeof", "$", "while", "for", "fn", "return", "break", "continue", "namespace", "in", "out", "ref"
+                "using", "struct", "union", "enum", "bitfield", "be", "le", "if", "else", "false", "true", "this", "parent", "addressof", "sizeof", "$", "while", "for", "fn", "return", "break", "continue", "namespace", "in", "out", "ref", "null", "const"
             };
             for (auto &k : keywords)
                 langDef.mKeywords.insert(k);
