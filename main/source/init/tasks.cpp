7a9899a3b6292b7b138dcade6472287f0be688c4
iTrooz <hey@itrooz.fr>
2023-03-23 22:08:00 +0100

    Only execute the migration function in Linux

--- main/source/init/tasks.cpp
+++ main/source/init/tasks.cpp
@@ -463,7 +463,9 @@ namespace hex::init {
         return {
             { "Setting up environment",  setupEnvironment,    false },
             { "Creating directories",    createDirectories,   false },
+            #if defined(OS_LINUX)
             { "Migrate config to .config", migrateConfig,     false },
+            #endif
             { "Loading settings",        loadSettings,        false },
             { "Loading plugins",         loadPlugins,         false },
             { "Checking for updates",    checkForUpdates,     true  },
