e9c449e20ded8d13f8e10856e11339bcc7d20813
iTrooz <hey@itrooz.fr>
2023-05-01 23:44:06 +0200

    add a border to the button

--- lib/libimhex/source/ui/imgui_imhex_extensions.cpp
+++ lib/libimhex/source/ui/imgui_imhex_extensions.cpp
@@ -703,10 +703,12 @@ namespace ImGui {
         ImGui::PushStyleColor(ImGuiCol_Button, ImGui::GetCustomColorU32(ImGuiCustomCol_DescButton));
         ImGui::PushStyleColor(ImGuiCol_Text, ImGui::GetColorU32(ImGuiCol_ButtonActive));
         ImGui::PushStyleColor(ImGuiCol_ButtonActive, ImGui::GetCustomColorU32(ImGuiCustomCol_DescButtonActive));
+        ImGui::PushStyleVar(ImGuiStyleVar_FrameBorderSize, 1);
 
         bool res = ImGui::Button(label);
 
         ImGui::PopStyleColor(4);
+        ImGui::PopStyleVar(1);
         
         return res;
     }
