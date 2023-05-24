57dce121e03e21aacf578480c1b6a1a8885adbcb
iTrooz <hey@itrooz.fr>
2023-05-24 14:37:17 +0200

    add documentation to RecentEntry

--- plugins/builtin/include/content/recent.hpp
+++ plugins/builtin/include/content/recent.hpp
@@ -9,11 +9,32 @@
 #include <wolv/io/fs.hpp>
 
 namespace hex::plugin::builtin::recent {
+    
+    /**
+     * @brief Structure used to represent a recent other
+     */
     struct RecentEntry {
+        
+        /**
+         * @brief Name that should be used to display the entry to the user
+         */
         std::string displayName;
+
+        /**
+         * @brief type of this entry. Might be a provider id (e.g. hex.builtin.provider.file)
+         * or "project" in case of a project
+         */
         std::string type;
+
+        /**
+         * @brief path of this entry file
+         */
         std::fs::path filePath;
 
+        /**
+         * @brief Entire json data of the recent entry (include the fields above)
+         * Used for custom settings set by the providers
+         */
         nlohmann::json data;
 
         bool operator==(const RecentEntry &other) const {
@@ -35,10 +56,26 @@ namespace hex::plugin::builtin::recent {
     };
 
     void registerEventHandlers();
+
+    /**
+     * @brief Scan the files in ImHexPath::Recent to get the recent entries, and delete duplicates.
+     */
     void updateRecentEntries();
+    
+    /**
+     * @brief Load a recent entry in ImHex. The entry might be a provider of a project
+     * @param recentEntry entry to load
+     */
     void loadRecentEntry(const RecentEntry &recentEntry);
 
+    /**
+     * @brief Draw the recent providers in the welcome screen
+     */
     void draw();
+
+    /**
+     * @brief Draw the "open recent" item in the "File" menu
+     */
     void drawFileMenuItem();
 
 }
