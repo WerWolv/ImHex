8729eaf444b2db4c693429cbbef7797e50eb6a66
iTrooz <hey@itrooz.fr>
2023-05-24 09:49:15 +0200

    Show which commit was used to compile ImHex on CLI

--- main/source/main.cpp
+++ main/source/main.cpp
@@ -38,6 +38,9 @@ int main(int argc, char **argv, char **envp) {
             Window::initNative();
 
             hex::log::info("Welcome to ImHex {}!", IMHEX_VERSION);
+            #if defined(GIT_BRANCH) && defined(GIT_COMMIT_HASH_SHORT)
+                hex::log::info("Compiled using commit {}", hex::format("{0}@{1}", GIT_BRANCH, GIT_COMMIT_HASH_SHORT));
+            #endif
 
             init::WindowSplash splashWindow;
 
