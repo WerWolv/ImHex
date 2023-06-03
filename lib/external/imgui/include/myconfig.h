9a5c3f48b9a3c86864333aaab5f962af1d618bb0
iTrooz <hey@itrooz.fr>
2023-06-03 18:24:30 +0200

    fix redefine errors by storing options in a custom config header
    I have no idea why it fixes it

---
+++ lib/external/imgui/include/myconfig.h
@@ -0,0 +1,9 @@
+// ImGui config (check imconfig.h for more information about these definitions)
+#pragma once
+
+#define IMGUI_DEFINE_MATH_OPERATORS
+#define IMGUI_DISABLE_OBSOLETE_FUNCTIONS
+#define IMGUI_DISABLE_OBSOLETE_KEYIO
+#define IMGUI_ENABLE_FREETYPE
+#define ImDrawIdx unsigned int
+#define IMGUI_DEBUG_TOOL_ITEM_PICKER_EX
