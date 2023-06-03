7b0c75c071958bc185566ea2b9b247dcca1ba923
iTrooz <hey@itrooz.fr>
2023-06-03 16:32:36 +0200

    re-add warnings

--- cmake/build_helpers.cmake
+++ cmake/build_helpers.cmake
@@ -412,7 +412,7 @@ function(downloadImHexPatternsFiles dest)
 endfunction()
 
 macro(setupCompilerWarnings target)
-    # set(IMHEX_COMMON_FLAGS "-Wall -Wextra -Wpedantic -Werror")
+    set(IMHEX_COMMON_FLAGS "-Wall -Wextra -Wpedantic -Werror")
     set(IMHEX_C_FLAGS "${IMHEX_COMMON_FLAGS} -Wno-restrict -Wno-stringop-overread -Wno-stringop-overflow -Wno-array-bounds -Wno-dangling-reference")
 
     set(CMAKE_C_FLAGS    "${CMAKE_C_FLAGS}    ${IMHEX_C_FLAGS}")
