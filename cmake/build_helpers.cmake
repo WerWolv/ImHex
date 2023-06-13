05a024336dc7889054db573dae6c4fce55b9869b
KOLANICH <kolan_n@mail.ru>
2023-06-13 20:18:26 +0300

    build: document usage of `pattern_language` from a subdir since there is no package for it since it is an internal lib.

--- cmake/build_helpers.cmake
+++ cmake/build_helpers.cmake
@@ -581,6 +581,7 @@ macro(addBundledLibraries)
 
     add_subdirectory(${EXTERN_LIBS_FOLDER}/pattern_language EXCLUDE_FROM_ALL)
     set_target_properties(libpl PROPERTIES POSITION_INDEPENDENT_CODE ON)
+    #list(APPEND badCloneExclusions "pattern_language")
 
     find_package(mbedTLS 2.26.0 REQUIRED)
 
