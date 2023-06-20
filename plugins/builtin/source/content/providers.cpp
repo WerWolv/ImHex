7cb62dbde6f8a9ed752efe8130b6ca2ffbd5330a
iTrooz <hey@itrooz.fr>
2023-06-20 10:00:24 +0200

    fix spacing in providers.cpp

--- plugins/builtin/source/content/providers.cpp
+++ plugins/builtin/source/content/providers.cpp
@@ -33,32 +33,32 @@ namespace hex::plugin::builtin {
         ContentRegistry::Provider::add<ViewProvider>(false);
 
         ProjectFile::registerHandler({
-             .basePath = "providers",
-             .required = true,
-             .load = [](const std::fs::path &basePath, Tar &tar) {
-                 auto json = nlohmann::json::parse(tar.readString(basePath / "providers.json"));
-                 auto providerIds = json.at("providers").get<std::vector<int>>();
+            .basePath = "providers",
+            .required = true,
+            .load = [](const std::fs::path &basePath, Tar &tar) {
+                auto json = nlohmann::json::parse(tar.readString(basePath / "providers.json"));
+                auto providerIds = json.at("providers").get<std::vector<int>>();
 
-                 bool success = true;
-                 std::map<hex::prv::Provider*, std::string> providerWarnings;
-                 for (const auto &id : providerIds) {
-                     auto providerSettings = nlohmann::json::parse(tar.readString(basePath / hex::format("{}.json", id)));
+                bool success = true;
+                std::map<hex::prv::Provider*, std::string> providerWarnings;
+                for (const auto &id : providerIds) {
+                    auto providerSettings = nlohmann::json::parse(tar.readString(basePath / hex::format("{}.json", id)));
 
-                     auto providerType = providerSettings.at("type").get<std::string>();
-                     auto provider = ImHexApi::Provider::createProvider(providerType, true, false);
-                     ON_SCOPE_EXIT {
-                         if (!success) {
-                             for (auto &task : TaskManager::getRunningTasks())
-                                 task->interrupt();
+                    auto providerType = providerSettings.at("type").get<std::string>();
+                    auto provider = ImHexApi::Provider::createProvider(providerType, true, false);
+                    ON_SCOPE_EXIT {
+                        if (!success) {
+                            for (auto &task : TaskManager::getRunningTasks())
+                                task->interrupt();
 
-                             TaskManager::runWhenTasksFinished([]{
-                                 for (auto provider : ImHexApi::Provider::getProviders())
+                            TaskManager::runWhenTasksFinished([]{
+                                for (auto provider : ImHexApi::Provider::getProviders())
                                     ImHexApi::Provider::remove(provider, true);
-                             });
-                         }
-                     };
+                            });
+                        }
+                    };
 
-                     if (provider == nullptr) {
+                    if (provider == nullptr) {
                         // if a provider is not created, it will be overwritten when saving the project,
                         // so we should prevent the project from loading at all
                         showError(hex::format("hex.builtin.popup.error.project.load"_lang,
@@ -66,15 +66,15 @@ namespace hex::plugin::builtin {
                         ));
                         success = false;
                         break;
-                     }
+                    }
 
-                     provider->setID(id);
-                     provider->loadSettings(providerSettings.at("settings"));
-                     if (!provider->open() || !provider->isAvailable() || !provider->isReadable()) {
+                    provider->setID(id);
+                    provider->loadSettings(providerSettings.at("settings"));
+                    if (!provider->open() || !provider->isAvailable() || !provider->isReadable()) {
                         providerWarnings[provider] = provider->getErrorMessage();
-                     } else
-                         EventManager::post<EventProviderOpened>(provider);
-                 }
+                    } else
+                        EventManager::post<EventProviderOpened>(provider);
+                }
 
                 std::string warningMsg;
                 for(const auto &warning : providerWarnings){
@@ -85,7 +85,7 @@ namespace hex::plugin::builtin {
 
                 // if no providers were opened, display an error with
                 // the warnings that happened when opening them 
-                 if (ImHexApi::Provider::getProviders().size() == 0) {
+                if (ImHexApi::Provider::getProviders().size() == 0) {
                     showError(hex::format("hex.builtin.popup.error.project.load"_lang,
                         hex::format("hex.builtin.popup.error.project.load.no_providers"_lang)) + warningMsg);
 
@@ -99,27 +99,27 @@ namespace hex::plugin::builtin {
                             hex::format("hex.builtin.popup.error.project.load.some_providers_failed"_lang, warningMsg));
                     }
                     return success;
-                 }
-             },
-             .store = [](const std::fs::path &basePath, Tar &tar) {
-                 std::vector<int> providerIds;
-                 for (const auto &provider : ImHexApi::Provider::getProviders()) {
-                     auto id = provider->getID();
-                     providerIds.push_back(id);
+                }
+            },
+            .store = [](const std::fs::path &basePath, Tar &tar) {
+                std::vector<int> providerIds;
+                for (const auto &provider : ImHexApi::Provider::getProviders()) {
+                    auto id = provider->getID();
+                    providerIds.push_back(id);
 
-                     nlohmann::json json;
-                     json["type"] = provider->getTypeName();
-                     json["settings"] = provider->storeSettings();
+                    nlohmann::json json;
+                    json["type"] = provider->getTypeName();
+                    json["settings"] = provider->storeSettings();
 
-                     tar.writeString(basePath / hex::format("{}.json", id), json.dump(4));
-                 }
+                    tar.writeString(basePath / hex::format("{}.json", id), json.dump(4));
+                }
 
-                 tar.writeString(basePath / "providers.json",
+                tar.writeString(basePath / "providers.json",
                     nlohmann::json({ { "providers", providerIds } }).dump(4)
-                 );
+                );
 
-                 return true;
-             }
+                return true;
+            }
         });
     }
 
