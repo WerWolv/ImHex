72b1686cc9faa04ccbd6e1b84d3466c4486c8d70
WerWolv <werwolv98@gmail.com>
2023-06-24 22:56:30 +0200

    feat: Added basic C# template

---
+++ plugins/script_loader/templates/CSharp/ImHexScript/Program.cs
@@ -0,0 +1,13 @@
+﻿using ImHex;
+using System.Drawing;
+
+class Script
+{
+    public static void Main()
+    {
+        string? result = UI.ShowInputTextBox("Title", "Message", 100);
+
+        if (result != null)
+            UI.ShowMessageBox("Hello World Result: " + result);
+    }
+}
