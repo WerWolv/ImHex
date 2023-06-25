601083ae94ac7019212e82de457ea12ad7bcd0c7
WerWolv <werwolv98@gmail.com>
2023-06-25 23:07:23 +0200

    impr: Clean up .NET template

--- plugins/script_loader/templates/CSharp/ImHexScript/Program.cs
+++ plugins/script_loader/templates/CSharp/ImHexScript/Program.cs
@@ -5,9 +5,6 @@ class Script
 {
     public static void Main()
     {
-        string? result = UI.ShowInputTextBox("Title", "Message", 100);
 
-        if (result != null)
-            UI.ShowMessageBox("Hello World Result: " + result);
     }
 }
