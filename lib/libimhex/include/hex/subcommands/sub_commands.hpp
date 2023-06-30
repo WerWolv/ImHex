33f6f697dc4a7cf76e9f8b90565191ac1ea4e526
iTrooz <hey@itrooz.fr>
2023-06-30 21:21:52 +0200

    add documentation to processArguments()

--- lib/libimhex/include/hex/subcommands/sub_commands.hpp
+++ lib/libimhex/include/hex/subcommands/sub_commands.hpp
@@ -1,3 +1,8 @@
 namespace hex::init {
+    /**
+     * determine which subcommand to run from the args given, and run it.
+     * In some cases, the subcommand might exit the program (e.g. --help)
+     * and so this function might not return
+     */
     void processArguments(std::vector<std::string> args);
 }
