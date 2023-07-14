aa0f4c42cfe43d3d4b319b3c98f0035447fe0733
WerWolv <werwolv98@gmail.com>
2023-07-14 22:40:36 +0200

    build: Properly install curl

--- dist/get_deps_msys2.sh
+++ dist/get_deps_msys2.sh
@@ -4,10 +4,10 @@ pacman -S --needed --noconfirm  \
   mingw-w64-x86_64-gcc          \
   mingw-w64-x86_64-lld          \
   mingw-w64-x86_64-cmake        \
-  mingw-w64-x86_64-make         \
   mingw-w64-x86_64-ccache       \
   mingw-w64-x86_64-glfw         \
   mingw-w64-x86_64-file         \
+  mingw-w64-x86_64-curl         \
   mingw-w64-x86_64-mbedtls      \
   mingw-w64-x86_64-freetype     \
   mingw-w64-x86_64-dlfcn        \
