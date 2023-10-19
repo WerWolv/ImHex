79e25b0889e78a9130054b3ee58ddacd524c371e
WerWolv <werwolv98@gmail.com>
2023-10-19 22:08:39 +0200

    feat: Added bitwise shift left/right data processor nodes

--- plugins/builtin/source/content/data_processor_nodes.cpp
+++ plugins/builtin/source/content/data_processor_nodes.cpp
@@ -376,6 +376,63 @@ namespace hex::plugin::builtin {
         }
     };
 
+    class NodeBitwiseShiftLeft : public dp::Node {
+    public:
+        NodeBitwiseShiftLeft() : Node("hex.builtin.nodes.bitwise.shift_left.header", {
+                dp::Attribute(dp::Attribute::IOType::In, dp::Attribute::Type::Buffer, "hex.builtin.nodes.common.input"),
+                dp::Attribute(dp::Attribute::IOType::In, dp::Attribute::Type::Integer, "hex.builtin.nodes.common.amount"),
+                dp::Attribute(dp::Attribute::IOType::Out, dp::Attribute::Type::Buffer, "hex.builtin.nodes.common.output")
+        }) { }
+
+        void process() override {
+            const auto &input  = this->getBufferOnInput(0);
+            const auto &amount = this->getIntegerOnInput(1);
+
+            std::vector<u8> output = input;
+
+            for (u32 i = 0; i < amount; i += 1) {
+                u8 prevByte = 0x00;
+                for (auto &byte : output) {
+                    auto startValue = byte;
+
+                    byte <<= 1;
+                    byte |= (prevByte & 0x80) >> 7;
+                    prevByte = startValue;
+                }
+            }
+
+            this->setBufferOnOutput(2, output);
+        }
+    };
+
+    class NodeBitwiseShiftRight : public dp::Node {
+    public:
+        NodeBitwiseShiftRight() : Node("hex.builtin.nodes.bitwise.shift_left.header", {
+                dp::Attribute(dp::Attribute::IOType::In, dp::Attribute::Type::Buffer, "hex.builtin.nodes.common.input"),
+                dp::Attribute(dp::Attribute::IOType::In, dp::Attribute::Type::Integer, "hex.builtin.nodes.common.amount"),
+                dp::Attribute(dp::Attribute::IOType::Out, dp::Attribute::Type::Buffer, "hex.builtin.nodes.common.output")
+        }) { }
+
+        void process() override {
+            const auto &input  = this->getBufferOnInput(0);
+            const auto &amount = this->getIntegerOnInput(1);
+
+            std::vector<u8> output = input;
+
+            for (u32 i = 0; i < amount; i += 1) {
+                u8 prevByte = 0x00;
+                for (auto &byte : output | std::views::reverse) {
+                    auto startValue = byte;
+                    byte >>= 1;
+                    byte |= (prevByte & 0x01) << 7;
+                    prevByte = startValue;
+                }
+            }
+
+            this->setBufferOnOutput(2, output);
+        }
+    };
+
     class NodeBitwiseADD : public dp::Node {
     public:
         NodeBitwiseADD() : Node("hex.builtin.nodes.bitwise.add.header", { dp::Attribute(dp::Attribute::IOType::In, dp::Attribute::Type::Buffer, "hex.builtin.nodes.common.input.a"), dp::Attribute(dp::Attribute::IOType::In, dp::Attribute::Type::Buffer, "hex.builtin.nodes.common.input.b"), dp::Attribute(dp::Attribute::IOType::Out, dp::Attribute::Type::Buffer, "hex.builtin.nodes.common.output") }) { }
@@ -1325,6 +1382,8 @@ namespace hex::plugin::builtin {
         ContentRegistry::DataProcessorNode::add<NodeBitwiseOR>("hex.builtin.nodes.bitwise", "hex.builtin.nodes.bitwise.or");
         ContentRegistry::DataProcessorNode::add<NodeBitwiseXOR>("hex.builtin.nodes.bitwise", "hex.builtin.nodes.bitwise.xor");
         ContentRegistry::DataProcessorNode::add<NodeBitwiseNOT>("hex.builtin.nodes.bitwise", "hex.builtin.nodes.bitwise.not");
+        ContentRegistry::DataProcessorNode::add<NodeBitwiseShiftLeft>("hex.builtin.nodes.bitwise", "hex.builtin.nodes.bitwise.shift_left");
+        ContentRegistry::DataProcessorNode::add<NodeBitwiseShiftRight>("hex.builtin.nodes.bitwise", "hex.builtin.nodes.bitwise.shift_right");
         ContentRegistry::DataProcessorNode::add<NodeBitwiseSwap>("hex.builtin.nodes.bitwise", "hex.builtin.nodes.bitwise.swap");
 
         ContentRegistry::DataProcessorNode::add<NodeDecodingBase64>("hex.builtin.nodes.decoding", "hex.builtin.nodes.decoding.base64");
