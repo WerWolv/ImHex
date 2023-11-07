WerWolv/ImHex

commit c839ee7d13c2a3f375f0711d5b8213d8cac4a2dd

build: Improve names of packaging cmake options (#1398)
This PR is simply a refactor, to clarify these flags that shouldn't
impact anything

cmake/build_helpers.cmake
@@ -89,14 +89,6 @@ endmacro()
 
 
 macro(configurePackingResources)
-    set(IMHEX_FORCE_LINK_PLUGINS "")
-
-    option (CREATE_PACKAGE "Create a package with CPack" OFF)
-
-    if (APPLE)
-        option (CREATE_BUNDLE "Create a bundle on macOS" OFF)
-    endif()
-
     if (WIN32)
         if (NOT (CMAKE_BUILD_TYPE STREQUAL "Debug"))
             set(APPLICATION_TYPE WIN32)
@@ -104,7 +96,7 @@ macro(configurePackingResources)
 
         set(IMHEX_ICON "${IMHEX_BASE_FOLDER}/resources/resource.rc")
 
-        if (CREATE_PACKAGE)
+        if (IMHEX_GENERATE_PACKAGE)
             set(CPACK_GENERATOR "WIX")
             set(CPACK_PACKAGE_NAME "ImHex")
             set(CPACK_PACKAGE_VENDOR "WerWolv")
@@ -122,7 +114,7 @@ macro(configurePackingResources)
     elseif (APPLE)
         set (IMHEX_ICON "${IMHEX_BASE_FOLDER}/resources/dist/macos/AppIcon.icns")
 
-        if (CREATE_BUNDLE)
+        if (IMHEX_GENERATE_PACKAGE)
             set(APPLICATION_TYPE MACOSX_BUNDLE)
             set_source_files_properties(${IMHEX_ICON} PROPERTIES MACOSX_PACKAGE_LOCATION "Resources")
             set(MACOSX_BUNDLE_ICON_FILE "AppIcon.icns")
@@ -170,7 +162,7 @@ macro(createPackage)
                 if (WIN32)
                     install(TARGETS ${plugin} RUNTIME DESTINATION ${PLUGINS_INSTALL_LOCATION})
                 elseif (APPLE)
-                    if (CREATE_BUNDLE)
+                    if (IMHEX_GENERATE_PACKAGE)
                         set_target_properties(${plugin} PROPERTIES LIBRARY_OUTPUT_DIRECTORY ${PLUGINS_INSTALL_LOCATION})
                     else ()
                         set_target_properties(${plugin} PROPERTIES LIBRARY_OUTPUT_DIRECTORY ${CMAKE_BINARY_DIR}/plugins)
@@ -240,7 +232,7 @@ macro(createPackage)
 
     endif()
     
-    if (CREATE_BUNDLE)
+    if (IMHEX_GENERATE_PACKAGE AND APPLE)
         include(PostprocessBundle)
         
         set_target_properties(libimhex PROPERTIES SOVERSION ${IMHEX_VERSION})
@@ -272,7 +264,7 @@ macro(createPackage)
         endif()
     endif()
 
-    if (CREATE_PACKAGE)
+    if (IMHEX_GENERATE_PACKAGE AND WIN32)
         set (CPACK_BUNDLE_NAME "ImHex")
         set (CPACK_BUNDLE_ICON "${CMAKE_SOURCE_DIR}/resources/dist/macos/AppIcon.icns" )
         set (CPACK_BUNDLE_PLIST "${CMAKE_BINARY_DIR}/ImHex.app/Contents/Info.plist")

.github/workflows/build.yml
@@ -78,7 +78,7 @@ jobs:
         cmake -G "Ninja"                                \
           -DCMAKE_BUILD_TYPE=${{env.BUILD_TYPE}}        \
           -DCMAKE_INSTALL_PREFIX="$PWD/install"         \
-          -DCREATE_PACKAGE=ON                           \
+          -DIMHEX_GENERATE_PACKAGE=ON                   \
           -DIMHEX_USE_DEFAULT_BUILD_SETTINGS=ON         \
           -DIMHEX_PATTERNS_PULL_MASTER=ON               \
           -DIMHEX_COMMIT_HASH_SHORT="${GITHUB_SHA::7}"  \
@@ -214,8 +214,7 @@ jobs:
         PKG_CONFIG_PATH="$(brew --prefix openssl)/lib/pkgconfig":"$(brew --prefix)/lib/pkgconfig"   \
         cmake -G "Ninja"                                                                            \
           -DCMAKE_BUILD_TYPE=${{env.BUILD_TYPE}}                                                    \
-          -DCREATE_BUNDLE=ON                                                                        \
-          -DCREATE_PACKAGE=ON                                                                       \
+          -DIMHEX_GENERATE_PACKAGE=ON                                                               \
           -DCMAKE_C_COMPILER_LAUNCHER=ccache                                                        \
           -DCMAKE_CXX_COMPILER_LAUNCHER=ccache                                                      \
           -DCMAKE_OBJC_COMPILER_LAUNCHER=ccache                                                     \

CMakeLists.txt
@@ -14,6 +14,7 @@ option(IMHEX_ENABLE_LTO "Enables Link Time Optimizations if possible" OFF)
 option(IMHEX_USE_DEFAULT_BUILD_SETTINGS "Use default build settings" OFF)
 option(IMHEX_STRICT_WARNINGS "Enable most available warnings and treat them as errors" ON)
 option(IMHEX_STATIC_LINK_PLUGINS "Statically link all plugins into the main executable" OFF)
+option(IMHEX_GENERATE_PACKAGE "Specify if a native package should be built. Only usable on Windows and MacOS" OFF)
 
 # Basic compiler and cmake configurations
 set(CMAKE_CXX_STANDARD 23)

commit 6e6c5c4cb73676f948e5bc94c89fb45c2cbc8d68

build: Fix build script for MacOS (#1404)
Fix build problem introduced by
https://github.com/WerWolv/ImHex/pull/1398
I didn't refactor the code well because I thought `CREATE_PACKAGE` was
an option only used for Windows, when it needs to be used for MacOS too

cmake/build_helpers.cmake
@@ -257,6 +257,9 @@ macro(createPackage)
 
         # Enforce DragNDrop packaging.
         set(CPACK_GENERATOR "DragNDrop")
+
+        set (CPACK_BUNDLE_ICON "${CMAKE_SOURCE_DIR}/resources/dist/macos/AppIcon.icns" )
+        set (CPACK_BUNDLE_PLIST "${CMAKE_BINARY_DIR}/ImHex.app/Contents/Info.plist")
     else()
         install(TARGETS main RUNTIME DESTINATION ${CMAKE_INSTALL_BINDIR})
         if(WIN32) # Forwarder is only needed on Windows
@@ -264,10 +267,8 @@ macro(createPackage)
         endif()
     endif()
 
-    if (IMHEX_GENERATE_PACKAGE AND WIN32)
+    if (IMHEX_GENERATE_PACKAGE)
         set (CPACK_BUNDLE_NAME "ImHex")
-        set (CPACK_BUNDLE_ICON "${CMAKE_SOURCE_DIR}/resources/dist/macos/AppIcon.icns" )
-        set (CPACK_BUNDLE_PLIST "${CMAKE_BINARY_DIR}/ImHex.app/Contents/Info.plist")
 
         include(CPack)
     endif()