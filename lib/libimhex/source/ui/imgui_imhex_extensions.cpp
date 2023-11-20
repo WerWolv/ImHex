3da209b5623ff2ddfa0c392a11ae642bbed484ab
WerWolv <werwolv98@gmail.com>
2023-11-20 23:51:50 +0100

    fix: Web build not working anymore

--- lib/libimhex/source/ui/imgui_imhex_extensions.cpp
+++ lib/libimhex/source/ui/imgui_imhex_extensions.cpp
@@ -6,7 +6,14 @@
 
 #include <imgui.h>
 #include <imgui_internal.h>
-#include <imgui_impl_opengl3_loader.h>
+
+#if defined(OS_WEB)
+    #define GLFW_INCLUDE_ES3
+    #include <GLES3/gl3.h>
+#else
+    #include <imgui_impl_opengl3_loader.h>
+#endif
+
 #undef IMGUI_DEFINE_MATH_OPERATORS
 
 #define STB_IMAGE_IMPLEMENTATION
