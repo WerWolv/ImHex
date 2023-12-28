03dc26d2d41ac6f28edfca8d50a19b3b8f1ea5d4
WerWolv <werwolv98@gmail.com>
2023-12-28 19:24:56 +0100

    build: Always extract magic file database

--- plugins/builtin/source/content/file_extraction.cpp
+++ plugins/builtin/source/content/file_extraction.cpp
@@ -6,23 +6,32 @@
 namespace hex::plugin::builtin {
 
     void extractBundledFiles() {
-        for (const auto &romfsPath : romfs::list("auto_extract")) {
-            for (const auto &imhexPath : fs::getDataPaths()) {
-                const auto path = imhexPath / std::fs::relative(romfsPath, "auto_extract");
-                if (wolv::io::fs::exists(path))
-                    continue;
-
-                wolv::io::File file(path, wolv::io::File::Mode::Create);
-                if (!file.isValid())
-                    continue;
-
-                auto data = romfs::get(romfsPath).span<u8>();
-                file.writeBuffer(data.data(), data.size());
-
-                if (file.getSize() == data.size())
-                    break;
+        constexpr static std::array<std::pair<std::string_view, bool>, 2> Paths = {
+            {
+                { "auto_extract", false },
+                { "always_auto_extract", true }
             }
+        };
 
+        for (const auto [extractFolder, alwaysExtract] : Paths) {
+            for (const auto &romfsPath : romfs::list(extractFolder)) {
+                for (const auto &imhexPath : fs::getDataPaths()) {
+                    const auto path = imhexPath / std::fs::relative(romfsPath, extractFolder);
+                    if (!alwaysExtract && wolv::io::fs::exists(path))
+                        continue;
+
+                    wolv::io::File file(path, wolv::io::File::Mode::Create);
+                    if (!file.isValid())
+                        continue;
+
+                    auto data = romfs::get(romfsPath).span<u8>();
+                    file.writeBuffer(data.data(), data.size());
+
+                    if (file.getSize() == data.size())
+                        break;
+                }
+
+            }
         }
     }
 
