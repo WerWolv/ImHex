3f6ce9e8da0dab5b46143766c6e8cafd6d00fbdc
WerWolv <werwolv98@gmail.com>
2023-12-29 13:35:13 +0100

    Make all the libraries optional

--- plugins/decompress/source/content/pl_functions.cpp
+++ plugins/decompress/source/content/pl_functions.cpp
@@ -4,18 +4,27 @@
 #include <pl/core/evaluator.hpp>
 #include <pl/patterns/pattern.hpp>
 
-#include <archive.h>
-#include <archive_entry.h>
-
 #include <wolv/utils/guards.hpp>
 
 #include <vector>
 #include <optional>
 
-#include <zlib.h>
-#include <bzlib.h>
-#include <lzma.h>
-#include <zstd.h>
+#if defined (LIBARCHIVE_FOUND)
+    #include <archive.h>
+    #include <archive_entry.h>
+#endif
+#if defined (ZLIB_FOUND)
+    #include <zlib.h>
+#endif
+#if defined (BZIP2_FOUND)
+    #include <bzlib.h>
+#endif
+#if defined (LIBLZMA_FOUND)
+    #include <lzma.h>
+#endif
+#if defined (ZSTD_FOUND)
+    #include <zstd.h>
+#endif
 
 namespace hex::plugin::decompress {
 
@@ -41,193 +50,221 @@ namespace hex::plugin::decompress {
 
         /* decompress() */
         ContentRegistry::PatternLanguage::addFunction(nsHexDec, "decompress", FunctionParameterCount::exactly(2), [](Evaluator *evaluator, auto params) -> std::optional<Token::Literal> {
-            auto compressedData = getCompressedData(evaluator, params[0]);
-            auto &section = evaluator->getSection(params[1].toUnsigned());
+            #if defined (LIBARCHIVE_FOUND)
+                auto compressedData = getCompressedData(evaluator, params[0]);
+                auto &section = evaluator->getSection(params[1].toUnsigned());
 
-            auto inArchive = archive_read_new();
-            ON_SCOPE_EXIT {
-                archive_read_close(inArchive);
-                archive_read_free(inArchive);
-            };
+                auto inArchive = archive_read_new();
+                if (inArchive == nullptr)
+                    return false;
 
-            archive_read_support_filter_gzip(inArchive);
-            archive_read_support_format_all(inArchive);
+                ON_SCOPE_EXIT {
+                    archive_read_close(inArchive);
+                    archive_read_free(inArchive);
+                };
 
-            archive_read_open_memory(inArchive, compressedData.data(), compressedData.size());
+                archive_read_support_filter_gzip(inArchive);
+                archive_read_support_format_all(inArchive);
 
-            archive_entry *entry = nullptr;
-            while (archive_read_next_header(inArchive, &entry) == ARCHIVE_OK) {
-                const void *block = nullptr;
-                size_t size = 0x00;
-                i64 offset = 0x00;
+                archive_read_open_memory(inArchive, compressedData.data(), compressedData.size());
 
-                while (archive_read_data_block(inArchive, &block, &size, &offset) == ARCHIVE_OK) {
-                    section.resize(section.size() + size);
-                    std::memcpy(section.data(), block, size);
+                archive_entry *entry = nullptr;
+                while (archive_read_next_header(inArchive, &entry) == ARCHIVE_OK) {
+                    const void *block = nullptr;
+                    size_t size = 0x00;
+                    i64 offset = 0x00;
+
+                    while (archive_read_data_block(inArchive, &block, &size, &offset) == ARCHIVE_OK) {
+                        section.resize(section.size() + size);
+                        std::memcpy(section.data(), block, size);
+                    }
                 }
-            }
 
-            return std::nullopt;
+                return true;
+            #else
+                err::E0012.throwError("hex::dec::decompress is not available. Please recompile with libarchive support.");
+                return false;
+            #endif
         });
 
         ContentRegistry::PatternLanguage::addFunction(nsHexDec, "zlib_decompress", FunctionParameterCount::exactly(2), [](Evaluator *evaluator, auto params) -> std::optional<Token::Literal> {
-            auto compressedData = getCompressedData(evaluator, params[0]);
-            auto &section = evaluator->getSection(params[1].toUnsigned());
+            #if defined (ZLIB_FOUND)
+                auto compressedData = getCompressedData(evaluator, params[0]);
+                auto &section = evaluator->getSection(params[1].toUnsigned());
 
-            z_stream stream = { };
-            if (inflateInit(&stream) != Z_OK) {
-                return std::nullopt;
-            }
-
-            section.resize(100);
-
-            stream.avail_in = compressedData.size();
-            stream.avail_out = section.size();
-            stream.next_in = compressedData.data();
-            stream.next_out = section.data();
-
-            ON_SCOPE_EXIT {
-                inflateEnd(&stream);
-            };
-
-            while (stream.avail_in != 0) {
-                auto res = inflate(&stream, Z_NO_FLUSH);
-                if (res == Z_STREAM_END) {
-                    section.resize(section.size() - stream.avail_out);
-                    break;
+                z_stream stream = { };
+                if (inflateInit(&stream) != Z_OK) {
+                    return false;
                 }
-                if (res != Z_OK)
-                    return std::nullopt;
 
-                if (stream.avail_out != 0)
-                    break;
+                section.resize(100);
 
-                section.resize(section.size() * 2);
-                stream.next_out  = section.data();
+                stream.avail_in = compressedData.size();
                 stream.avail_out = section.size();
-            }
+                stream.next_in = compressedData.data();
+                stream.next_out = section.data();
 
-            return std::nullopt;
+                ON_SCOPE_EXIT {
+                    inflateEnd(&stream);
+                };
+
+                while (stream.avail_in != 0) {
+                    auto res = inflate(&stream, Z_NO_FLUSH);
+                    if (res == Z_STREAM_END) {
+                        section.resize(section.size() - stream.avail_out);
+                        break;
+                    }
+                    if (res != Z_OK)
+                        return false;
+
+                    if (stream.avail_out != 0)
+                        break;
+
+                    section.resize(section.size() * 2);
+                    stream.next_out  = section.data();
+                    stream.avail_out = section.size();
+                }
+
+                return true;
+            #else
+                err::E0012.throwError("hex::dec::zlib_decompress is not available. Please recompile with zlib support.");
+                return false;
+            #endif
         });
 
         ContentRegistry::PatternLanguage::addFunction(nsHexDec, "bzlib_decompress", FunctionParameterCount::exactly(2), [](Evaluator *evaluator, auto params) -> std::optional<Token::Literal> {
-            auto compressedData = getCompressedData(evaluator, params[0]);
-            auto &section = evaluator->getSection(params[1].toUnsigned());
+            #if defined(BZIP2_FOUND)
+                auto compressedData = getCompressedData(evaluator, params[0]);
+                auto &section = evaluator->getSection(params[1].toUnsigned());
 
-            bz_stream stream = { };
-            if (BZ2_bzDecompressInit(&stream, 0, 1) != Z_OK) {
-                return std::nullopt;
-            }
-
-            section.resize(100);
-
-            stream.avail_in  = compressedData.size();
-            stream.avail_out = section.size();
-            stream.next_in   = reinterpret_cast<char*>(compressedData.data());
-            stream.next_out  = reinterpret_cast<char*>(section.data());
-
-            ON_SCOPE_EXIT {
-                BZ2_bzDecompressEnd(&stream);
-            };
-
-            while (stream.avail_in != 0) {
-                auto res = BZ2_bzDecompress(&stream);
-                if (res == BZ_STREAM_END) {
-                    section.resize(section.size() - stream.avail_out);
-                    break;
+                bz_stream stream = { };
+                if (BZ2_bzDecompressInit(&stream, 0, 1) != Z_OK) {
+                    return false;
                 }
-                if (res != BZ_OK)
-                    return std::nullopt;
 
-                if (stream.avail_out != 0)
-                    break;
+                section.resize(100);
 
-                section.resize(section.size() * 2);
-                stream.next_out  = reinterpret_cast<char*>(section.data());
+                stream.avail_in  = compressedData.size();
                 stream.avail_out = section.size();
-            }
+                stream.next_in   = reinterpret_cast<char*>(compressedData.data());
+                stream.next_out  = reinterpret_cast<char*>(section.data());
 
-            return std::nullopt;
+                ON_SCOPE_EXIT {
+                    BZ2_bzDecompressEnd(&stream);
+                };
+
+                while (stream.avail_in != 0) {
+                    auto res = BZ2_bzDecompress(&stream);
+                    if (res == BZ_STREAM_END) {
+                        section.resize(section.size() - stream.avail_out);
+                        break;
+                    }
+                    if (res != BZ_OK)
+                        return false;
+
+                    if (stream.avail_out != 0)
+                        break;
+
+                    section.resize(section.size() * 2);
+                    stream.next_out  = reinterpret_cast<char*>(section.data());
+                    stream.avail_out = section.size();
+                }
+
+                return true;
+            #else
+                err::E0012.throwError("hex::dec::bzlib_decompress is not available. Please recompile with bzip2 support.");
+                return false;
+            #endif
+
         });
 
         ContentRegistry::PatternLanguage::addFunction(nsHexDec, "lzma_decompress", FunctionParameterCount::exactly(2), [](Evaluator *evaluator, auto params) -> std::optional<Token::Literal> {
-            auto compressedData = getCompressedData(evaluator, params[0]);
-            auto &section = evaluator->getSection(params[1].toUnsigned());
+            #if defined (LIBLZMA_FOUND)
+                auto compressedData = getCompressedData(evaluator, params[0]);
+                auto &section = evaluator->getSection(params[1].toUnsigned());
 
-            lzma_stream stream = LZMA_STREAM_INIT;
-            if (lzma_auto_decoder(&stream, 0x10000, LZMA_IGNORE_CHECK) != Z_OK) {
-                return std::nullopt;
-            }
-
-            section.resize(100);
-
-            stream.avail_in = compressedData.size();
-            stream.avail_out = section.size();
-            stream.next_in = compressedData.data();
-            stream.next_out = section.data();
-
-            ON_SCOPE_EXIT {
-                lzma_end(&stream);
-            };
-
-            while (stream.avail_in != 0) {
-                auto res = lzma_code(&stream, LZMA_RUN);
-                if (res == BZ_STREAM_END) {
-                    section.resize(section.size() - stream.avail_out);
-                    break;
+                lzma_stream stream = LZMA_STREAM_INIT;
+                if (lzma_auto_decoder(&stream, 0x10000, LZMA_IGNORE_CHECK) != Z_OK) {
+                    return false;
                 }
-                if (res != LZMA_OK && res != LZMA_STREAM_END)
-                    return std::nullopt;
 
-                if (stream.avail_out != 0)
-                    break;
+                section.resize(100);
 
-                section.resize(section.size() * 2);
-                stream.next_out = compressedData.data();
-                stream.avail_out = compressedData.size();
-            }
+                stream.avail_in = compressedData.size();
+                stream.avail_out = section.size();
+                stream.next_in = compressedData.data();
+                stream.next_out = section.data();
 
-            return std::nullopt;
+                ON_SCOPE_EXIT {
+                    lzma_end(&stream);
+                };
+
+                while (stream.avail_in != 0) {
+                    auto res = lzma_code(&stream, LZMA_RUN);
+                    if (res == BZ_STREAM_END) {
+                        section.resize(section.size() - stream.avail_out);
+                        break;
+                    }
+                    if (res != LZMA_OK && res != LZMA_STREAM_END)
+                        return false;
+
+                    if (stream.avail_out != 0)
+                        break;
+
+                    section.resize(section.size() * 2);
+                    stream.next_out = compressedData.data();
+                    stream.avail_out = compressedData.size();
+                }
+
+                return true;
+            #else
+                err::E0012.throwError("hex::dec::lzma_decompress is not available. Please recompile with liblzma support.");
+                return false;
+            #endif
         });
 
         ContentRegistry::PatternLanguage::addFunction(nsHexDec, "zstd_decompress", FunctionParameterCount::exactly(2), [](Evaluator *evaluator, auto params) -> std::optional<Token::Literal> {
-            auto compressedData = getCompressedData(evaluator, params[0]);
-            auto &section = evaluator->getSection(params[1].toUnsigned());
+            #if defined (ZSTD_FOUND)
+                auto compressedData = getCompressedData(evaluator, params[0]);
+                auto &section = evaluator->getSection(params[1].toUnsigned());
 
-            ZSTD_DCtx* dctx = ZSTD_createDCtx();
-            if (dctx == nullptr) {
-                return std::nullopt;
-            }
+                ZSTD_DCtx* dctx = ZSTD_createDCtx();
+                if (dctx == nullptr) {
+                    return false;
+                }
 
-            ON_SCOPE_EXIT {
-                ZSTD_freeDCtx(dctx);
-            };
-
-            const u8* source = compressedData.data();
-            size_t sourceSize = compressedData.size();
-
-            do {
-                size_t blockSize = ZSTD_getFrameContentSize(source, sourceSize);
-
-                if (blockSize == ZSTD_CONTENTSIZE_ERROR) {
+                ON_SCOPE_EXIT {
                     ZSTD_freeDCtx(dctx);
-                    return std::nullopt;
-                }
+                };
 
-                section.resize(section.size() + blockSize);
+                const u8* source = compressedData.data();
+                size_t sourceSize = compressedData.size();
 
-                size_t decodedSize = ZSTD_decompressDCtx(dctx, section.data() + section.size() - blockSize, blockSize, source, sourceSize);
+                do {
+                    size_t blockSize = ZSTD_getFrameContentSize(source, sourceSize);
 
-                if (ZSTD_isError(decodedSize)) {
-                    return std::nullopt;
-                }
+                    if (blockSize == ZSTD_CONTENTSIZE_ERROR) {
+                        return false;
+                    }
 
-                source = source + sourceSize;
-                sourceSize = 0;
+                    section.resize(section.size() + blockSize);
 
-            } while (sourceSize > 0);
+                    size_t decodedSize = ZSTD_decompressDCtx(dctx, section.data() + section.size() - blockSize, blockSize, source, sourceSize);
 
-            return std::nullopt;
+                    if (ZSTD_isError(decodedSize)) {
+                        return false;
+                    }
+
+                    source = source + sourceSize;
+                    sourceSize = 0;
+
+                } while (sourceSize > 0);
+
+                return true;
+            #else
+                err::E0012.throwError("hex::dec::zstd_decompress is not available. Please recompile with zstd support.");
+                return false;
+            #endif
         });
     }
 
