e3ec4ebd210e57f2399b232232ef1893a86a2bee
WerWolv <werwolv98@gmail.com>
2024-01-05 14:47:30 +0100

    build: Make msys2 deps install script use pacboy

--- dist/get_deps_msys2.sh
+++ dist/get_deps_msys2.sh
@@ -1,19 +1,20 @@
 #!/usr/bin/env sh
 
-pacman -S --needed --noconfirm  \
-  mingw-w64-x86_64-gcc          \
-  mingw-w64-x86_64-lld          \
-  mingw-w64-x86_64-cmake        \
-  mingw-w64-x86_64-ccache       \
-  mingw-w64-x86_64-glfw         \
-  mingw-w64-x86_64-file         \
-  mingw-w64-x86_64-curl-winssl  \
-  mingw-w64-x86_64-mbedtls      \
-  mingw-w64-x86_64-freetype     \
-  mingw-w64-x86_64-dlfcn        \
-  mingw-w64-x86_64-ninja        \
-  mingw-w64-x86_64-capstone     \
-  mingw-w64-x86_64-zlib         \
-  mingw-w64-x86_64-bzip2        \
-  mingw-w64-x86_64-xz           \
-  mingw-w64-x86_64-zstd
+pacman -S --needed --noconfirm pactoys
+pacboy -S --needed --noconfirm  \
+  gcc:p          \
+  lld:p          \
+  cmake:p        \
+  ccache:p       \
+  glfw:p         \
+  file:p         \
+  curl-winssl:p  \
+  mbedtls:p      \
+  freetype:p     \
+  dlfcn:p        \
+  ninja:p        \
+  capstone:p     \
+  zlib:p         \
+  bzip2:p        \
+  xz:p           \
+  zstd:p
