f6c332b8fd02e891c84d2e6b9509c4eb339de0a2
WerWolv <werwolv98@gmail.com>
2024-01-25 20:03:56 +0100

    Fix link errors with capstone and curl

--- cmake/modules/FindCapstone.cmake
+++ cmake/modules/FindCapstone.cmake
@@ -1,17 +1,8 @@
-find_path(CAPSTONE_INCLUDE_DIR
-        capstone.h
-        HINTS ${PC_CAPSTONE_INCLUDEDIR} ${PC_CAPSTONE_INCLUDE_DIRS}
-        PATH_SUFFIXES capstone
-)
-
-find_library(CAPSTONE_LIBRARY
-        NAMES capstone
-        HINTS ${PC_CAPSTONE_LIBDIR} ${PC_CAPSTONE_LIBRARY_DIRS}
-)
+find_path(CAPSTONE_INCLUDE_DIR capstone.h)
+find_library(CAPSTONE_LIBRARY NAMES capstone)
 
 include(FindPackageHandleStandardArgs)
 
-find_package_handle_standard_args(Capstone DEFAULT_MSG
-        CAPSTONE_LIBRARY CAPSTONE_INCLUDE_DIR)
+find_package_handle_standard_args(Capstone DEFAULT_MSG CAPSTONE_LIBRARY CAPSTONE_INCLUDE_DIR)
 
 mark_as_advanced(CAPSTONE_INCLUDE_DIR CAPSTONE_LIBRARY)
