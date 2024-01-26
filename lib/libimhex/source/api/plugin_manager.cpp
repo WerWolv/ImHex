4c06fd2fb83109a051415677da06053356045149
WerWolv <werwolv98@gmail.com>
2024-01-26 21:11:56 +0100

    impr: Allow plugins to be loaded in debug builds

--- lib/libimhex/source/api/plugin_manager.cpp
+++ lib/libimhex/source/api/plugin_manager.cpp
@@ -107,7 +107,8 @@ namespace hex {
 
 
         const auto requestedVersion = getCompatibleVersion();
-        if (requestedVersion != ImHexApi::System::getImHexVersion()) {
+        const auto imhexVersion = ImHexApi::System::getImHexVersion();
+        if (!imhexVersion.starts_with(requestedVersion)) {
             if (requestedVersion.empty()) {
                 log::warn("Plugin '{}' did not specify a compatible version, assuming it is compatible with the current version of ImHex.", wolv::util::toUTF8String(m_path.filename()));
             } else {
