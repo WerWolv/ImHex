7bf76b62c25be0edbd53e85b2e5e2550357baed1
iTrooz <hey@itrooz.fr>
2024-02-07 13:44:24 +0100

    add simple read/write test for builtin (memory) provider

--- plugins/builtin/tests/source/main.cpp
+++ plugins/builtin/tests/source/main.cpp
@@ -1,6 +1,30 @@
 #include <hex/test/tests.hpp>
+#include <hex/api/plugin_manager.hpp>
+#include <content/providers/memory_file_provider.hpp>
+#include <content/views/view_patches.hpp>
 
-TEST_SEQUENCE("Test1") {
-    TEST_ASSERT(1 == 1, "1 should be equal to 1");
+using namespace hex;
+using namespace hex::plugin::builtin;
+
+TEST_SEQUENCE("Providers/ReadWrite") {
+    ViewPatches view; // TODO remove this hack
+    MemoryFileProvider pr;
+
+    TEST_ASSERT(pr.getSize() == 0);
+    TEST_ASSERT(!pr.isDirty());
+
+    pr.resize(50);
+    TEST_ASSERT(pr.getSize() == 50);
+    TEST_ASSERT(pr.isDirty());
+
+    char buf[] = "\x99\x99"; // temporary value that should be overwriten
+    pr.read(0, buf, 2);
+    TEST_ASSERT(std::equal(buf, buf+2, "\x00\x00"));
+
+    pr.write(0, "\xFF\xFF", 2);
+    char buf2[] = "\x99\x99"; // temporary value that should be overwriten
+    pr.read(0, buf2, 2);
+    TEST_ASSERT(std::equal(buf2, buf2+2, "\xFF\xFF"));
+
     TEST_SUCCESS();
 };
