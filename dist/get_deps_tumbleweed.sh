bf2e7c0be8680344f074ad7c52ebe6a0cb47cac0
Kihau <michal.haldas@gmail.com>
2024-03-23 15:41:02 +0100

    Add dependency download script for OpenSUSE Tumbleweed

---
+++ dist/get_deps_tumbleweed.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env sh
+
+zypper install     \
+  cmake            \
+  ninja            \
+  gcc12            \
+  gcc12-c++        \
+  freetype2-devel  \
+  libcurl-devel    \
+  dbus-1-devel     \
+  file-devel       \
+  Mesa-libGL-devel \
+  libglfw-devel    \
+  mbedtls-devel    \
+  gtk3-devel       \
+  libzstd-devel    \
+  zlib-devel       \
+  bzip3-devel      \
+  xz-devel
