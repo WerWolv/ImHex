696612385a530de7d7b13b05e156483c85c7a845
WerWolv <werwolv98@gmail.com>
2024-03-29 16:34:01 +0100

    fix: Use `std::ranges::find` instead of `std::ranges::contains` for now

--- plugins/builtin/source/content/ui_items.cpp
+++ plugins/builtin/source/content/ui_items.cpp
@@ -465,7 +465,8 @@ namespace hex::plugin::builtin {
                         auto &tabProvider = providers[i];
                         const auto selectedProviderIndex = ImHexApi::Provider::getCurrentProviderIndex();
 
-                        if (std::ranges::contains(ImHexApi::Provider::impl::getClosingProviders(), tabProvider))
+                        const auto &closingProviders = ImHexApi::Provider::impl::getClosingProviders();
+                        if (std::ranges::find(closingProviders, tabProvider) != closingProviders.end())
                             continue;
 
                         bool open = true;
