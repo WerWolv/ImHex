f5e84d908d4ef52dfe62a8c5028ce823ffb76e81
David Mentler <david.mentler@shapr3d.com>
2024-05-18 14:50:51 +0200

    Xcode accomodating build, allow sourcing ImHex-Patterns from sibling directory

--- cmake/modules/ImHexPlugin.cmake
+++ cmake/modules/ImHexPlugin.cmake
@@ -55,7 +55,7 @@ macro(add_imhex_plugin)
     # Configure build properties
     set_target_properties(${IMHEX_PLUGIN_NAME}
             PROPERTIES
-            RUNTIME_OUTPUT_DIRECTORY ${CMAKE_BINARY_DIR}/plugins
+            RUNTIME_OUTPUT_DIRECTORY "${IMHEX_MAIN_OUTPUT_DIRECTORY}/plugins"
             CXX_STANDARD 23
             PREFIX ""
             SUFFIX ${IMHEX_PLUGIN_SUFFIX}
