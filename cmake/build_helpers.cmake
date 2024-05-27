92b1234ddb248e599f0404cd74b9b3e739d46665
iTrooz <hey@itrooz.fr>
2024-05-27 16:33:15 +0200

    build: Reduce debug info produced (#1720)

--- cmake/build_helpers.cmake
+++ cmake/build_helpers.cmake
@@ -600,6 +600,15 @@ macro(setupCompilerFlags target)
     set(CMAKE_C_FLAGS    "${CMAKE_C_FLAGS}    ${IMHEX_COMMON_FLAGS} ${IMHEX_C_CXX_FLAGS}")
     set(CMAKE_CXX_FLAGS  "${CMAKE_CXX_FLAGS}  ${IMHEX_COMMON_FLAGS} ${IMHEX_C_CXX_FLAGS}  ${IMHEX_CXX_FLAGS}")
     set(CMAKE_OBJC_FLAGS "${CMAKE_OBJC_FLAGS} ${IMHEX_COMMON_FLAGS}")
+
+    # Only generate minimal debug information for stacktraces in RelWithDebInfo builds
+    set(CMAKE_C_FLAGS_RELWITHDEBINFO "${CMAKE_C_FLAGS_RELWITHDEBINFO} -g1")
+    set(CMAKE_CXX_FLAGS_RELWITHDEBINFO "${CMAKE_CXX_FLAGS_RELWITHDEBINFO} -g1")
+    if (CMAKE_CXX_COMPILER_ID STREQUAL "GNU")
+        # Add flags for debug info in inline functions
+        set(CMAKE_C_FLAGS_RELWITHDEBINFO "${CMAKE_C_FLAGS_RELWITHDEBINFO} -gstatement-frontiers -ginline-points")
+        set(CMAKE_CXX_FLAGS_RELWITHDEBINFO "${CMAKE_CXX_FLAGS_RELWITHDEBINFO} -gstatement-frontiers -ginline-points")
+    endif()
 endmacro()
 
 # uninstall target
