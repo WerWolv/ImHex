ce26fe1db7b7ba1ad69603cd7bbb814973c80ffa
WerWolv <werwolv98@gmail.com>
2024-07-27 11:23:21 +0200

    impr: Move language string interpretation to compile time

--- lib/libimhex/source/api/localization_manager.cpp
+++ lib/libimhex/source/api/localization_manager.cpp
@@ -10,7 +10,7 @@ namespace hex {
 
             AutoReset<std::string> s_fallbackLanguage;
             AutoReset<std::string> s_selectedLanguage;
-            AutoReset<std::map<std::string, std::string>> s_currStrings;
+            AutoReset<std::map<size_t, std::string>> s_currStrings;
 
         }
 
@@ -41,6 +41,21 @@ namespace hex {
             return m_entries;
         }
 
+        static void loadLanguageDefinitions(const std::vector<LanguageDefinition> &definitions) {
+            for (const auto &definition : definitions) {
+                const auto &entries = definition.getEntries();
+                if (entries.empty())
+                    continue;
+
+                for (const auto &[key, value] : entries) {
+                    if (value.empty())
+                        continue;
+
+                    s_currStrings->emplace(Lang::hash(key), value);
+                }
+            }
+        }
+
         void loadLanguage(const std::string &language) {
             auto &definitions = ContentRegistry::Language::impl::getLanguageDefinitions();
 
@@ -49,14 +64,10 @@ namespace hex {
 
             s_currStrings->clear();
 
-            for (const auto &definition : definitions.at(language))
-                s_currStrings->insert(definition.getEntries().begin(), definition.getEntries().end());
+            loadLanguageDefinitions(definitions.at(language));
 
             const auto& fallbackLanguage = getFallbackLanguage();
-            if (language != fallbackLanguage && definitions.contains(fallbackLanguage)) {
-                for (const auto &definition : definitions.at(fallbackLanguage))
-                    s_currStrings->insert(definition.getEntries().begin(), definition.getEntries().end());
-            }
+            loadLanguageDefinitions(definitions.at(fallbackLanguage));
 
             s_selectedLanguage = language;
         }
@@ -98,11 +109,10 @@ namespace hex {
 
     }
 
-    Lang::Lang(const char *unlocalizedString) : m_unlocalizedString(unlocalizedString) { }
-    Lang::Lang(const std::string &unlocalizedString) : m_unlocalizedString(unlocalizedString) { }
-    Lang::Lang(const UnlocalizedString &unlocalizedString) : m_unlocalizedString(unlocalizedString.get()) { }
-    Lang::Lang(std::string_view unlocalizedString) : m_unlocalizedString(unlocalizedString) { }
-
+    Lang::Lang(const char *unlocalizedString) : m_entryHash(hash(unlocalizedString)) { }
+    Lang::Lang(const std::string &unlocalizedString) : m_entryHash(hash(unlocalizedString)) { }
+    Lang::Lang(const UnlocalizedString &unlocalizedString) : m_entryHash(hash(unlocalizedString.get())) { }
+    Lang::Lang(std::string_view unlocalizedString) : m_entryHash(hash(unlocalizedString)) { }
 
     Lang::operator std::string() const {
         return get();
@@ -114,6 +124,18 @@ namespace hex {
 
     Lang::operator const char *() const {
         return get().c_str();
+    }
+
+    const std::string &Lang::get() const {
+        const auto &lang = *LocalizationManager::s_currStrings;
+
+        auto it = lang.find(m_entryHash);
+        if (it == lang.end()) {
+            static const std::string invalidString = "[ !!! INVALID LANGUAGE STRING !!! ]";
+            return invalidString;
+        } else {
+            return it->second;
+        }
     }
 
     std::string operator+(const std::string &&left, const Lang &&right) {
@@ -142,14 +164,6 @@ namespace hex {
 
     std::string operator+(const Lang &&left, const char *right) {
         return static_cast<std::string>(left) + right;
-    }
-
-    const std::string &Lang::get() const {
-        auto &lang = LocalizationManager::s_currStrings;
-        if (lang->contains(m_unlocalizedString))
-            return lang->at(m_unlocalizedString);
-        else
-            return m_unlocalizedString;
     }
 
 }
