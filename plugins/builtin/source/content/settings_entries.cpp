1a54e08f11ba1d573c61db0fb11c6e7a5b736dd2
WerWolv <werwolv98@gmail.com>
2025-01-14 17:50:48 +0100

    fix: Remove constexpr from functions that really aren't constexpr

--- plugins/builtin/source/content/settings_entries.cpp
+++ plugins/builtin/source/content/settings_entries.cpp
@@ -721,11 +721,11 @@ namespace hex::plugin::builtin {
             }
 
         private:
-            constexpr static float pixelsToPoints(float pixels) {
+            static float pixelsToPoints(float pixels) {
                 return pixels * (72_scaled / 96.0F);
             }
 
-            constexpr static float pointsToPixels(float points) {
+            static float pointsToPixels(float points) {
                 return points / (72_scaled / 96.0F);
             }
         };
