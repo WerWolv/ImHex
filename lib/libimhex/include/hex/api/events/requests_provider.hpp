74db34ceb7d0c99207c59571469a203cd594075a
BioTheWolff <47079795+BioTheWolff@users.noreply.github.com>
2025-01-22 15:12:06 +0100

    docs: add documentation for lifecycle and provider requests
    Signed-off-by: BioTheWolff <47079795+BioTheWolff@users.noreply.github.com>

--- lib/libimhex/include/hex/api/events/requests_provider.hpp
+++ lib/libimhex/include/hex/api/events/requests_provider.hpp
@@ -1,20 +1,21 @@
 #pragma once
 
-#include <hex.hpp>
 #include <hex/api/event_manager.hpp>
 
-
+/* Provider requests definitions */
 namespace hex {
 
-
     /**
-     * @brief Creates a provider from it's unlocalized name, and add it to the provider list
+     * @brief Creates a provider from its unlocalized name, and add it to the provider list
     */
     EVENT_DEF(RequestCreateProvider, std::string, bool, bool, hex::prv::Provider **);
 
     /**
-     * Move the data from all PerProvider instances from one provider to another.
+     * @brief Move the data from all PerProvider instances from one provider to another
+     *
      * The 'from' provider should not have any per provider data after this, and should be immediately deleted
+     *
+     * FIXME: rename with the "Request" prefix to apply standard naming convention.
     */
     EVENT_DEF(MovePerProviderData, prv::Provider *, prv::Provider *);
 
