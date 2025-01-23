de49d645daf2751c4f6347c2cc015cfc40a34725
BioTheWolff <47079795+BioTheWolff@users.noreply.github.com>
2025-01-23 07:58:03 +0100

    fix: add GUI requests include for windows plugin
    Signed-off-by: BioTheWolff <47079795+BioTheWolff@users.noreply.github.com>

--- plugins/windows/source/plugin_windows.cpp
+++ plugins/windows/source/plugin_windows.cpp
@@ -4,6 +4,7 @@
 #include <hex/api/theme_manager.hpp>
 #include <hex/helpers/logger.hpp>
 #include <hex/api/events/events_gui.hpp>
+#include <hex/api/events/requests_gui.hpp>
 
 #include <romfs/romfs.hpp>
 #include <nlohmann/json.hpp>
