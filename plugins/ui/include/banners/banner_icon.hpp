673e7089b5d2d373a855bf4f49fc23c7d03cdaad
paxcut <paxcut@outlook.com>
2025-01-30 08:32:50 -0700

    fix: Banner window scaling
    The banner windows did not scale with the fonts resulting in cropped text when font size was made bigger than normal.
    fixed by ensuring the window is big enough and then making sure text is centered in the y-axis.

--- plugins/ui/include/banners/banner_icon.hpp
+++ plugins/ui/include/banners/banner_icon.hpp
@@ -11,6 +11,9 @@ namespace hex::ui {
                 : Banner(color), m_icon(icon), m_message(std::move(message)) { }
 
         void drawContent() override {
+            auto textHeight = std::max(ImGui::CalcTextSize(Lang(m_message)).y, ImGui::CalcTextSize(m_icon).y);
+            auto textOffset = (ImGui::GetWindowHeight() - textHeight) / 2;
+            ImGui::SetCursorPosY(ImGui::GetCursorPosY() + textOffset);
             ImGui::TextUnformatted(m_icon);
             ImGui::SameLine(0, 10_scaled);
             ImGui::TextUnformatted(Lang(m_message));
