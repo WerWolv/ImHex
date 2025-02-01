483468a6c79a6325354435a8ea71017cad217518
WerWolv <werwolv98@gmail.com>
2025-02-01 16:02:45 +0100

    impr: Make WorkspaceManager internals static

--- lib/libimhex/source/api/workspace_manager.cpp
+++ lib/libimhex/source/api/workspace_manager.cpp
@@ -14,10 +14,10 @@
 
 namespace hex {
 
-    AutoReset<std::map<std::string, WorkspaceManager::Workspace>> s_workspaces;
-    decltype(s_workspaces)::Type::iterator s_currentWorkspace  = s_workspaces->end();
-    decltype(s_workspaces)::Type::iterator s_previousWorkspace = s_workspaces->end();
-    decltype(s_workspaces)::Type::iterator s_workspaceToRemove = s_workspaces->end();
+    static AutoReset<std::map<std::string, WorkspaceManager::Workspace>> s_workspaces;
+    static decltype(s_workspaces)::Type::iterator s_currentWorkspace  = s_workspaces->end();
+    static decltype(s_workspaces)::Type::iterator s_previousWorkspace = s_workspaces->end();
+    static decltype(s_workspaces)::Type::iterator s_workspaceToRemove = s_workspaces->end();
 
     void WorkspaceManager::createWorkspace(const std::string& name, const std::string &layout) {
         s_currentWorkspace = s_workspaces->insert_or_assign(name, Workspace {
