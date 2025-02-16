658d4c4d72f5681f512f3e170a0d881fdc16ca9b
WerWolv <werwolv98@gmail.com>
2025-02-16 23:46:08 +0100

    fix: Make sure fps counter values are initialized to zero

--- plugins/builtin/source/content/ui_items.cpp
+++ plugins/builtin/source/content/ui_items.cpp
@@ -248,7 +248,7 @@ namespace hex::plugin::builtin {
                             }, nullptr);
                             ImPlot::SetupAxisTicks(ImAxis_Y1, 0, largestFrameTime * 1.25F, 3);
 
-                            static std::vector<double> values(100);
+                            static std::vector<double> values(100, 0.0);
 
                             values.push_back(ImHexApi::System::getLastFrameTime());
                             if (values.size() > 100)
