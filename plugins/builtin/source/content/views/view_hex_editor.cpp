c8e51570b9cc4925ceec9616279f37ca5e8c36f0
Artem Garmash <a.garmash.dev@gmail.com>
2025-04-28 10:00:58 +0400

    Extract menu items enabling logic

--- plugins/builtin/source/content/views/view_hex_editor.cpp
+++ plugins/builtin/source/content/views/view_hex_editor.cpp
@@ -1294,7 +1294,8 @@ namespace hex::plugin::builtin {
                                                         ui::ToastInfo::open("hex.builtin.tools.file_tools.shredder.success"_lang);
                                                     }
                                                 },
-                                                [] { return ImHexApi::Provider::isValid() && ImHexApi::HexEditor::isSelectionValid() && ImHexApi::HexEditor::getSelection()->getSize() == 1; });
+                                                canSearchForDifferingByte);
+
 
         ContentRegistry::Interface::addMenuItem({ "hex.builtin.menu.file", "Next differing byte" }, ICON_VS_DEBUG_STEP_INTO, 1600,
                                                 CTRLCMD + Keys::RightBracket,
@@ -1326,7 +1327,7 @@ namespace hex::plugin::builtin {
                                                         ui::ToastInfo::open("hex.builtin.tools.file_tools.shredder.success"_lang);
                                                     }
                                                 },
-                                                [] { return ImHexApi::Provider::isValid() && ImHexApi::HexEditor::isSelectionValid() && ImHexApi::HexEditor::getSelection()->getSize() == 1; });
+                                                canSearchForDifferingByte);
 
 
         ContentRegistry::Interface::addMenuItemSeparator({ "hex.builtin.menu.edit" }, 1100, this);
