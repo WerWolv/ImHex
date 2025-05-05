4a35fe5ab2bd673aacf6b8639e9649d22885de33
Artem Garmash <a.garmash.dev@gmail.com>
2025-05-05 19:00:06 +0200

    Improve parameter naming

--- plugins/builtin/include/content/differing_byte_searcher.hpp
+++ plugins/builtin/include/content/differing_byte_searcher.hpp
@@ -7,7 +7,7 @@
 namespace hex::plugin::builtin {
 
     void findNextDifferingByte(
-        const std::function< u64(prv::Provider*) >& endAddressProvider,
+        const std::function< u64(prv::Provider*) >& lastValidAddressProvider,
         const std::function< bool(u64, u64) >& addressComparator,
         const std::function< void(u64*) >& addressStepper,
         bool *didFindNextValue,
