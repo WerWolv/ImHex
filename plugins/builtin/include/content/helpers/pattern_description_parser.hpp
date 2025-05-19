f2f004fd90a40da570d4aa95cbc455b887c32098
shewitt.au <shewitt.au@gmail.com>
2025-05-20 04:09:11 +1000

    Comments

--- plugins/builtin/include/content/helpers/pattern_description_parser.hpp
+++ plugins/builtin/include/content/helpers/pattern_description_parser.hpp
@@ -1,4 +1,9 @@
 #pragma once
+/*
+A standalone hexpat file parser to extract the ‘#pragma description’.
+Used to speed up “Import Pattern File…” in the “Pattern Editor” by avoiding
+having to preprocess every pattern using pattern_language to extract the it.
+*/
 
 #include <optional>
 #include <string>
--- plugins/builtin/source/content/helpers/pattern_description_parser.cpp
+++ plugins/builtin/source/content/helpers/pattern_description_parser.cpp
@@ -1,3 +1,9 @@
+/*
+A standalone hexpat file parser to extract the ‘#pragma description’.
+Used to speed up “Import Pattern File…” in the “Pattern Editor” by avoiding
+having to preprocess every pattern using pattern_language to extract the it.
+*/
+
 #include <ctype.h>
 #include <string>
 #include <optional>
