1ed90b3f69322b491a4ee7aefc0e61e1b0bfd9e1
Nik <werwolv98@gmail.com>
2025-08-09 19:57:16 +0200

    Small fixes

--- main/gui/source/init/tasks.cpp
+++ main/gui/source/init/tasks.cpp
@@ -20,6 +20,7 @@
 
 #include <wolv/io/fs.hpp>
 #include <wolv/io/file.hpp>
+#include <wolv/utils/string.hpp>
 
 namespace hex::init {
 
@@ -203,13 +204,15 @@ namespace hex::init {
         auto keepNewest = [&](u32 count, const paths::impl::DefaultPath &pathType) {
             for (const auto &path : pathType.write()) {
                 try {
-                    const auto canonicalPath = std::filesystem::canonical(path);        // path without simlinks and ".."
+                    const auto canonicalPath = std::filesystem::canonical(path);        
                     std::vector<std::filesystem::directory_entry> files;
 
-                    for (const auto& file : std::filesystem::directory_iterator(canonicalPath)){
-                        if(!file.is_regular_file()) continue;   // skip symlinks and directories
+                    for (const auto &file : std::filesystem::directory_iterator(canonicalPath)){
+                           // Skip symlinks and directories
+                        if (!file.is_regular_file()) 
+                            continue;
 
-                        if(std::filesystem::canonical(file.path()).native().starts_with(canonicalPath.native()))
+                        if (std::filesystem::canonical(file.path()).native().starts_with(canonicalPath.native()))
                             files.push_back(file);
                         else
                             log::warn("Skip file outside directory {}", file.path().string());
@@ -223,10 +226,11 @@ namespace hex::init {
                     });
 
                     for (auto it = files.begin() + count; it != files.end(); ++it){
-                        if(it->is_regular_file()) std::filesystem::remove(it->path());
+                        if (it->is_regular_file()) 
+                            std::filesystem::remove(it->path());
                     }
                 } catch (std::filesystem::filesystem_error &e) {
-                    log::error("Failed to clear old file in directory '{}'! {}", path.string(), e.what());
+                    log::error("Failed to clear old file in directory '{}'! {}", wolv::util::toUTF8String(path), e.what());
                     result = false;
                 }
             }
