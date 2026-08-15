cd2f04a04eb56d398f4d51fecae2bf213c34c87d
WerWolv <werwolv98@gmail.com>
2026-08-15 13:12:04 +0200

    fix: UCRT64 does not support any locale except for the C one

--- main/gui/source/main.cpp
+++ main/gui/source/main.cpp
@@ -21,6 +21,24 @@ namespace hex::init {
 
 }
 
+namespace {
+
+    void configureLocale() {
+        #if defined (OS_WINDOWS)
+            // MSYS2 UCRT64 runtime does not support any sort of locales except for the "C" locale.
+            // See https://github.com/gcc-mirror/gcc/blob/master/libstdc%2B%2B-v3/config/locale/generic/c_locale.cc#L245
+            std::setlocale(LC_ALL, "C");
+        #else
+            // Elsewhere, set the locale to en_US.UTF-8 to ensure that we can handle UTF-8 strings correctly
+            // falling back to the C locale if the rest isn't available
+            std::setlocale(LC_ALL, "en_US.UTF-8") ||
+            std::setlocale(LC_ALL, "C.UTF-8")     ||
+            std::setlocale(LC_ALL, "C");
+        #endif
+    }
+
+}
+
 /**
  * @brief Main entry point of ImHex
  * @param argc Argument count
@@ -30,12 +48,8 @@ namespace hex::init {
 int main(int argc, char **argv) {
     using namespace hex;
 
-    // Set the locale to some variant of en_US with UTF-8 encoding if possible at all
-    std::setlocale(LC_ALL, "en_US.UTF-8") ||
-    std::setlocale(LC_ALL, "en_US.utf-8") ||
-    std::setlocale(LC_ALL, "C.UTF-8")     ||
-    std::setlocale(LC_ALL, "C.utf-8")     ||
-    std::setlocale(LC_ALL, "C");
+    // Set the program locale
+    configureLocale();
 
     // Tell the Task Manager that we are the main thread
     TaskManager::setMainThreadId(std::this_thread::get_id());
