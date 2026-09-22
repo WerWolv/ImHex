84a32827a4c41d22cbe50b281f6686bcf9d73b1d
jiishiin <77448717+jiishiin@users.noreply.github.com>
2026-09-23 01:46:21 +0530

    feat: Added option to resize selected region (#2908)
    ### Implementation description
    Added a toggle button to allow resizing the selected region.
    
    ### Screenshots
    default resize mode:
    <img width="382" height="117" alt="image"
    src="https://github.com/user-attachments/assets/3a263617-93a8-46aa-ae31-8fa31ab5c667"
    />
    
    selection resize mode:
    <img width="382" height="117" alt="image"
    src="https://github.com/user-attachments/assets/3039d2f5-37e1-4300-b16d-db28359b45f2"
    />

--- plugins/builtin/source/content/popups/hex_editor/popup_hex_editor_resize.cpp
+++ plugins/builtin/source/content/popups/hex_editor/popup_hex_editor_resize.cpp
@@ -1,39 +1,79 @@
 #include "content/popups/hex_editor/popup_hex_editor_resize.hpp"
 #include "content/views/view_hex_editor.hpp"
 
-#include <hex/api/content_registry/settings.hpp>
-#include <hex/api/content_registry/user_interface.hpp>
-
-#include <content/differing_byte_searcher.hpp>
+#include <fonts/tabler_icons.hpp>
 
 namespace hex::plugin::builtin {
 
-    PopupResize::PopupResize(u64 currSize) : m_size(currSize) {}
+    PopupResize::PopupResize(const ImHexApi::HexEditor::ProviderRegion &selection) : m_selection(selection), m_actualSize(0), m_selectionSize(0), m_isSelection(false) {
+        if (selection.provider != nullptr) {
+            m_actualSize = selection.provider->getActualSize();
+            m_selectionSize = selection.size;
+        }
+    }
 
     void PopupResize::draw(ViewHexEditor *editor) {
-        ImGuiExt::InputHexadecimal("##resize", &m_size);
-        if (ImGui::IsItemFocused() && (ImGui::IsKeyPressed(ImGuiKey_Enter) || ImGui::IsKeyPressed(ImGuiKey_KeypadEnter))) {
-            this->resize(m_size);
+        // Resize input
+        ImGuiExt::InputHexadecimal("##resize", (m_isSelection ? &m_selectionSize : &m_actualSize), ImGuiInputTextFlags_AutoSelectAll);
+
+        ImGui::SameLine();
+
+        // Resize selection toggle
+        ImGui::BeginDisabled(m_selection.getSize() == 0);
+        {
+            ImGuiExt::DimmedIconToggle(ICON_TA_ARTICLE, ICON_TA_ARTICLE_OFF, &m_isSelection);
+            ImGuiExt::InfoTooltip("hex.builtin.view.hex_editor.menu.edit.resize_selection"_lang);
+        }
+        ImGui::EndDisabled();
+
+        ImGui::Spacing();
+        ImGui::Separator();
+
+        if (ImGui::IsWindowFocused() && (ImGui::IsKeyPressed(ImGuiKey_Enter) || ImGui::IsKeyPressed(ImGuiKey_KeypadEnter))) {
+            resizeProvider(editor);
             editor->closePopup();
         }
 
         ImGuiExt::ConfirmButtons("hex.ui.common.set"_lang, "hex.ui.common.cancel"_lang,
             [&, this]{
-                this->resize(m_size);
+                resizeProvider(editor);
                 editor->closePopup();
             },
             [&]{
                 editor->closePopup();
-            });
+            }
+        );
     }
 
     UnlocalizedString PopupResize::getTitle() const {
         return "hex.builtin.view.hex_editor.menu.edit.resize"_unlocalized;
     }
 
-    void PopupResize::resize(size_t newSize) {
-        if (ImHexApi::Provider::isValid())
-            ImHexApi::Provider::get()->resize(newSize);
-    }
+    void PopupResize::resizeProvider(ViewHexEditor *editor) {
+        if (m_selection.provider == nullptr) {
+            return;
+        }
 
+        if (m_isSelection) {
+            const auto resizeOffset = (m_selection.getEndAddress() - m_selection.provider->getBaseAddress());
+            const i64 sizeDiff = (static_cast<i64>(m_selectionSize) - static_cast<i64>(m_selection.size));
+
+            if (sizeDiff > 0) {
+                m_selection.provider->insert((resizeOffset + 1), sizeDiff);
+            } else if (sizeDiff < 0) {
+                m_selection.provider->remove((resizeOffset + sizeDiff + 1), -sizeDiff);
+            } else {
+                // Same size
+            }
+            m_selection.size = m_selectionSize;
+        } else {
+            m_selection.provider->resize(m_actualSize);
+            m_selection.address = m_selection.provider->getBaseAddress();
+            m_selection.size = m_actualSize;
+        }
+
+        // Handle selection
+        editor->setSelection(m_selection.getStartAddress(), m_selection.getEndAddress());
+        editor->setCursorPosition(m_selection.getEndAddress());
+    }
 }
